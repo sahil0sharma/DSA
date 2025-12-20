a6126a8b4dd267715990d602e4a638bd074de877
sahil0sharma <sahil0sharma5647@gmail.com>
2025-12-20 22:01:24 +0530

    A4

---
+++ src/Basic/Basic_concept/Math/A4.java
@@ -0,0 +1,27 @@
+package Basic.Basic_concept.Math;
+
+
+public class A4 {
+    public int Reverse_a_number(int n ){
+
+        if (n == 0) return 0;
+
+        int rev = 0;
+        while (n > 0){
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+
+            n = n / 10;
+
+        }
+        return rev;
+    }
+    public static void main(String[] args) {
+
+        A4 p4 = new A4();
+
+        int n = 1234;
+
+        System.out.println(p4.Reverse_a_number(n));
+    }
+}
