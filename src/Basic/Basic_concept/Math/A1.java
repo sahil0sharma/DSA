0972c590312b8972e41f8570b45177f0cf8f55dd
sahil0sharma <sahil0sharma5647@gmail.com>
2025-12-20 22:00:32 +0530

    A1

---
+++ src/Basic/Basic_concept/Math/A1.java
@@ -0,0 +1,27 @@
+package Basic.Basic_concept.Math;
+
+public class A1 {
+    public int countDigit(int n) {
+        int count = 0;
+
+        if (n == 0) {
+            return 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            count++;
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        int n = 1400;
+
+        A1 p1 = new A1();
+
+        System.out.println(p1.countDigit(n));
+
+    }
+
+}
