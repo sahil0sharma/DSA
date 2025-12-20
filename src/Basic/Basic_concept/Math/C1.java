91f6354afd4f6d0ee894181c3e520fbef863ca92
sahil0sharma <sahil0sharma5647@gmail.com>
2025-12-20 22:02:13 +0530

    C1

---
+++ src/Basic/Basic_concept/Math/C1.java
@@ -0,0 +1,24 @@
+package Basic.Basic_concept.Math;
+// Palindrome Number
+
+public class C1 {
+    public boolean isPalindrome(int n) {
+        int ncopy = n;
+
+        int rev = 0;
+        while ( ncopy > 0){
+            int last_digit = ncopy % 10;
+            rev = rev * 10 + last_digit;
+            ncopy /= 10;
+        }
+        return rev == n;
+    }
+
+    public static void main(String[] args) {
+        C1 c1 = new C1();
+
+        int n = 121;
+
+        System.out.println(c1.isPalindrome(n));
+    }
+}
