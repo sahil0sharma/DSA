62bb05b8e93a02d72f4902b63196bbb84e680aad
sahil0sharma <sahil0sharma5647@gmail.com>
2025-12-20 22:02:25 +0530

    D1

---
+++ src/Basic/Basic_concept/Math/D1.java
@@ -0,0 +1,24 @@
+package Basic.Basic_concept.Math;
+
+
+
+public class D1 {
+
+    public int factorial(int n) {
+        int fact = 1;
+        for( int i = 1; i <= n; i++){
+            // fact = fact * n;
+            // n = n - 1;
+            fact = fact * i;
+        }
+        return fact;
+    }
+    public static void main(String[] args){
+        D1 d1 = new D1();
+
+        int n = 3;
+
+        System.out.println(d1.factorial(n));
+
+    }
+}
