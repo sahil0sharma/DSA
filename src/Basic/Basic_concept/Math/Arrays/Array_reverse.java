77a25ebe8dc8033561291b19f15efa05e4127cc7
sahil0sharma <sahil0sharma5647@gmail.com>
2025-12-26 07:42:24 +0530

    Array problems

---
+++ src/Basic/Basic_concept/Math/Arrays/Array_reverse.java
@@ -0,0 +1,26 @@
+package Basic.Basic_concept.Math.Arrays;
+
+import java.util.Arrays;
+
+public class Array_reverse {
+
+    public void rev( int[] arr){
+
+
+        for( int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+
+        Array_reverse r = new Array_reverse();
+
+        int[] arr = { 1,2,3,4,5};
+
+     r.rev(arr);
+
+    }
+}
---
+++ src/Basic/Basic_concept/Math/Arrays/Array_sorted.java
@@ -0,0 +1,22 @@
+package Basic.Basic_concept.Math.Arrays;
+
+public class Array_sorted {
+
+        public boolean sorting(int[] arr){
+
+            for(int i = 0; i < arr.length - 1; i++){
+                for(int j = i + 1; j < arr.length; j++){
+                    if ( arr[i] > arr[j]){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    public static void main(String[] args) {
+        Array_sorted a = new Array_sorted();
+
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(a.sorting(arr));
+    }
+}
