610caea7bae386ddb455aaa54a187a4fccf56339
sahil0sharma <sahil0sharma5647@gmail.com>
2025-12-26 07:42:41 +0530

    Hashing problem

---
+++ src/Basic/Basic_concept/Math/Hashing/HA1Brute1.java
@@ -0,0 +1,37 @@
+package Basic.Basic_concept.Math.Hashing;
+
+public class HA1Brute1 {
+
+    public int hashing(int[] nums){
+
+        int n = nums.length;
+        int maxfreq = 0;
+        int maxEle = nums[0];
+
+        for( int i = 0; i < n; i++){
+            int freq = 0;
+
+            for( int j = 0; j < n ; j++){
+                if( nums[i] == nums[j]){
+                    freq++;
+                }
+                if ( freq > maxfreq){
+                    maxfreq = freq;
+                    maxEle = nums[i];
+                }
+                else if ( freq == maxfreq){
+                    maxEle = Math.min(maxEle, nums[i]);
+                }
+            }
+        }
+        return maxEle;
+    }
+    public static void main(String[] args) {
+
+        HA1Brute1 h1 = new HA1Brute1();
+        int[] arr = {1, 2, 3, 3, 2, 2};
+
+        System.out.println(h1.hashing(arr));
+
+    }
+}
---
+++ src/Basic/Basic_concept/Math/Hashing/HA1Brute2.java
@@ -0,0 +1,42 @@
+package Basic.Basic_concept.Math.Hashing;
+
+public class HA1Brute2 {
+
+    public int countHashing(int[] nums){
+
+        int n = nums.length;
+        int maxFreq = 0;
+        int maxEle = 0;
+
+        boolean[] visited = new boolean[n];
+
+        for( int i = 0; i < n; i++){
+            if (visited[i]) continue;
+
+            int freq = 0;
+
+            for( int j = 0; j < n; j++){
+                if(nums[i] == nums[j]){
+                    freq++;
+                    visited[j] = true;
+                }
+                if(freq > maxFreq){
+                    maxFreq = freq;
+                    maxEle = nums[i];
+                }
+                else if(freq == maxFreq){
+                    maxEle = Math.min(maxEle, nums[i]);
+                }
+            }
+        }
+        return maxEle;
+    }
+    public static void main(String[] args) {
+
+        HA1Brute2 h2 = new HA1Brute2();
+        int[] arr = {1, 2, 3 ,4 ,3 , 2,1,1,1};
+
+        System.out.println(h2.countHashing(arr));
+
+    }
+}
