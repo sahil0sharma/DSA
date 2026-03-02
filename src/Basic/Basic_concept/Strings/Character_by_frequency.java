0e2a2269cd0feb82b6f4c2a083b8127132b55ae3
sahil0sharma <sahil0sharma5647@gmail.com>
2026-03-02 20:17:20 +0530

    String

---
+++ src/Basic/Basic_concept/Strings/Character_by_frequency.java
@@ -0,0 +1,23 @@
+//package Basic.Basic_concept.Math.Strings;
+//
+//import java.util.*;
+//
+//public class Character_by_frequency {
+//
+//    public static char freq1(String s){
+//
+//        Map<Character, Integer> freq = new HashMap<>();
+//        for(int i = 0; i < s.length(); i++){
+//            char ch = s.charAt(i);
+//            freq.put(ch, freq.getOrDefault(ch, 0)+1);
+//        }
+//        int max = Integer.MIN_VALUE;
+//        int[] =
+//        for(Map.Entry<Character, Integer> i : freq.entrySet()){
+//            max = Math.max(i.getValue(), max);
+//
+//
+//        }
+//
+//        }
+//}
