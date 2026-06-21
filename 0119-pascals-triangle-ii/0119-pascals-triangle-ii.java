727cd200a0bb695b8cb0449baac494a67479418c
sahil0sharma <83935935+sahil0sharma@users.noreply.github.com>
2026-06-21 16:48:25 +0530

    Time: 0 ms (100%), Space: 42.4 MB (83.02%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,17 @@
+   class Solution {
+    public List<Integer> getRow(int rowIndex) {
+
+        List<Integer> list = new ArrayList<>();
+
+        long ans = 1;
+        list.add(1);
+
+        for(int i = 1; i <= rowIndex; i++){
+            ans = ans * (rowIndex - i + 1);
+            ans = ans / i;
+            list.add((int) ans);
+        }
+
+        return list;
+    }
+}
