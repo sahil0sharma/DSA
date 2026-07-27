0146df2e89e69d418e6fa52e86e4dc6ab354be1e
sahil0sharma <83935935+sahil0sharma@users.noreply.github.com>
2026-07-27 21:52:34 +0530

    Time: 0 ms (100%), Space: 43.6 MB (71.84%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,21 +1,23 @@
 class Solution {
     public void rotate(int[][] matrix) {
-       int[][] arr = new int[matrix.length][matrix.length];
+      int n = matrix.length;
 
-        int n = matrix.length - 1;
-
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix.length; j++){
-                arr[j][i] = matrix[n][j];
-            }
-            n--;
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++){
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
         }
+      }
 
-        // copy back into matrix
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix.length; j++){
-                matrix[i][j] = arr[i][j];
-            }
-        }   System.out.println(Arrays.deepToString(arr));
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < n/2; j++){
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][n- 1 -j];
+            matrix[i][n- 1 -j] = temp;
+        }
+      }
+
+      
     }
 }
