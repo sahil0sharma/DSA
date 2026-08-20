e4e59197a98df3db6c3c04b60393984418cda40f
sahil0sharma <83935935+sahil0sharma@users.noreply.github.com>
2026-08-20 09:17:09 +0530

    Time: 1 ms (98.08%), Space: 46.3 MB (95.43%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -5,15 +5,13 @@ class Solution {
         int[] arr1 = new int[n];
         int[] arr2 = new int[n];
 
-        // First operations
+   
         arr1[0] = nums[0];
         arr2[0] = nums[1];
 
-        // Pointers tracking the count / next empty index
         int a = 1;
         int b = 1;
 
-        // Distribute remaining elements
         for (int i = 2; i < n; i++) {
             if (arr1[a - 1] > arr2[b - 1]) {
                 arr1[a] = nums[i];
@@ -24,7 +22,7 @@ class Solution {
             }
         }
 
-        // Merge arr1 followed by arr2
+       
         int[] result = new int[n];
         int idx = 0;
 
