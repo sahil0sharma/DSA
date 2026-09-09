0744374ed99380d1edd69abe8b40922bc2b91cb5
sahil0sharma <83935935+sahil0sharma@users.noreply.github.com>
2026-09-09 21:34:31 +0530

    Time: 41 ms (26.76%), Space: 56.8 MB (75.42%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,26 +1,33 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int n = nums.length;
-        // count[s] stores how many times prefix sum 's' has occurred
-        int[] count = new int[n + 1];
-        count[0] = 1; // Base case: 0 odd numbers seen once initially
-        
-        int oddCount = 0;
-        int ans = 0;
-        
-        for (int num : nums) {
-            // Add 1 if odd, 0 if even
-            oddCount += (num % 2 != 0) ? 1 : 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+
+        int count = 0;
+        int prefix = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+          
+
             
-            // If we have at least k odds, check how many prefixes had (oddCount - k) odds
-            if (oddCount >= k) {
-                ans += count[oddCount - k];
-            }
+            if(nums[i] % 2 != 0){
+                prefix += 1;
+            } else {
+                prefix += 0;
+            } 
+
+            int need = prefix - k;
             
-            // Record current prefix sum frequency
-            count[oddCount]++;
+            count += map.getOrDefault(need, 0);
+
+            map.put(prefix, map.getOrDefault(prefix, 0)+1);
+
+
         }
-        
-        return ans;
+
+        return count;
+    
     }
 }
