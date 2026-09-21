12283216c6e9d31f73186021d3b81d61b163dd86
sahil0sharma <83935935+sahil0sharma@users.noreply.github.com>
2026-09-21 21:40:41 +0530

    Time: 2 ms (71.61%), Space: 47.3 MB (53.1%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,30 +1,25 @@
 class Solution {
     public int maxProduct(int[] nums) {
 
-        int n = nums.length;
-        
-        int ans = Integer.MIN_VALUE; // to store the answer
-        
-        // Indices to store the prefix and suffix multiplication
-        int prefix = 1, suffix = 1;
-        
-        // Iterate on the elements of the given array
-        for (int i = 0; i < n; i++) {
+        int cmax = nums[0];
+        int cmin = nums[0];
+
+        int maxp = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
             
-            /* Resetting the prefix and suffix
-            multiplication if they turn out to be zero */
-            if (prefix == 0) prefix = 1;
-            if (suffix == 0) suffix = 1;
-            
-            // update the prefix and suffix multiplication
-            prefix *= nums[i];
-            suffix *= nums[n - i - 1];
-            
-            // store the maximum as the answer
-            ans = Math.max(ans, Math.max(prefix, suffix));
+            int n = nums[i];
+
+            int tmax = Math.max(n, Math.max(cmax * n, cmin * n));
+
+            int tmin = Math.min(n, Math.min(cmax * n, cmin * n));
+
+            cmax = tmax;
+            cmin = tmin;
+
+            maxp = Math.max(maxp, cmax);
         }
-        
-        // return the result
-        return ans;
+
+        return maxp;
     }
 }
